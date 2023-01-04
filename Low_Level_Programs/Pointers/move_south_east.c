23df7ccd6df8157b7ee31c0cce4572d893ca0d86
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2023-01-04 11:05:26 -0500

    Starting on poiters

---
+++ Low_Level_Programs/Pointers/move_south_east.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void south_east( int *lat, int *lon){
+	*lat = *lat -1;
+	*lon = *lon +1;
+}
+
+int main(){
+	int latitude = 32;
+	int longitude = -73;
+
+	south_east(&latitude, &longitude);
+
+	printf("The new position is: [%i,%i]",latitude,longitude);
+	return 0;
+}
