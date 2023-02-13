b85882ad6c687c774b8cb7db8e2fb56037586ca3
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2023-02-13 16:23:54 -0500

    day 58

---
+++ Low_Level_Programs/Pointers/print_half.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+	void half(char *p)
+	{
+		int count;
+		int i;
+		int halff;
+		while(*(p + count) != '\0')
+		{
+			count++;
+		}
+		printf("\nNumber of characters are: %d",count);
+
+		halff = count/2;
+
+		printf("\nHalf number of characters should be: %d",halff);
+
+		putchar('\n');
+
+		for(i;i <=(count)/2; i++)
+		{
+			putchar(*(p + i));
+		}
+
+	}
+
+	char *str;
+
+	str = "Printing half of the following content";
+
+	half(str);
+
+	return 0;
+}
