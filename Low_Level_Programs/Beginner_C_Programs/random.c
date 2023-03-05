b89447110c59b6d7c5b7fec8ba0eaa0dee2b0704
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2023-03-05 20:29:58 +0000

    updates

--- Low_Level_Programs/Beginner_C_Programs/random.c
+++ Low_Level_Programs/Beginner_C_Programs/random.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ * Generating random numbers
+ */
+
 int main(void)
 {
 	int n;
