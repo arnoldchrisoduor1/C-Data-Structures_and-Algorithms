ad133494046fde82184392dd6e962c6faa86818e
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2023-04-05 20:13:12 +0000

    commenting

--- Low_Level_Programs/Beginner_C_Programs/islower.c
+++ Low_Level_Programs/Beginner_C_Programs/islower.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+/*
+ * This function checks if the letter
+ * is lowercase
+ * or uppercase
+ */
+
 int main(void){
 
 	int islower(int c){
 		if(c >= 'a' && c<='z')
 			printf("\n%c is a small letter",c);
-			else
-				printf("\n%c is a capital letter",c);
+		else
+			printf("\n%c is a capital letter",c);
 				  
-	
 	}
 
 	islower('c' + 0);
